82e0ba81ee7829125e7f9e63e455cbc1c7c1510b
mrif45 <muh.rifqi45@gmail.com>
2020-05-05 16:58:37 +0700

    after learning javascript

---
+++ ToggleMenu/main.js
@@ -0,0 +1,35 @@
+let toggleNavStatus = false;
+
+let toggleNav = function(){
+    let getSidebar = document.querySelector(".nav-sidebar");
+    let getSidebarUl = document.querySelector(".nav-sidebar ul");
+    let getSidebarTitle = document.querySelector(".nav-sidebar span");
+    let getSidebarLink = document.querySelectorAll(".nav-sidebar a");
+
+    if (toggleNavStatus === false){
+        getSidebarUl.style.visibility = "visible";
+        getSidebar.style.width = "272px";
+        getSidebarTitle.style.opacity = "0.5";
+
+        let arrayLength = getSidebarLink.length;
+        for (let i = 0; i < arrayLength; i++) {
+            getSidebarLink[i].style.opacity = "1";   
+        }
+
+        toggleNavStatus = true;
+    }
+    
+    else if (toggleNavStatus === true){
+        getSidebar.style.width = "50px";
+        getSidebarTitle.style.opacity = "0";
+
+        let arrayLength = getSidebarLink.length;
+        for (let i = 0; i < arrayLength; i++) {
+            getSidebarLink[i].style.opacity = "0";   
+        }
+
+        getSidebarUl.style.visibility = "hidden";
+
+        toggleNavStatus = false;
+    }
+}
